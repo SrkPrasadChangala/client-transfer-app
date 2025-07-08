006165ad1e2939f9ed0b85eb50c4ace07b25d605
Changala Siva Rama Krishna Prasad <77905636+SrkPrasadChangala@users.noreply.github.com>
2025-07-08 16:04:39 +0000

    initiated spring respository for backend using spring initialiser

---
+++ server/client-transfer-application/src/main/java/com/achtransfer/client_transfer_application/ClientTransferApplication.java
@@ -0,0 +1,13 @@
+package com.achtransfer.client_transfer_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientTransferApplication.class, args);
+	}
+
+}
---
+++ server/client-transfer-application/src/test/java/com/achtransfer/client_transfer_application/ClientTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achtransfer.client_transfer_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
